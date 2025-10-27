a97c3ce7ca3c2f9bdc9b81a3dffc4d92a64a3322
moshe mordechai diamandi <moshemor@g.jct.ac.il>
2025-10-27 17:46:33 +0200

    Initial Code + Welcome functions

---
+++ Stage0/Program3851.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome3851();
+        Console.ReadKey();
+    }
+
+    private static void Welcome3851()
+    {
+        Console.WriteLine("Enter your name: ");
+        string userName = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first console appliacation", userName);
+    }
+}
