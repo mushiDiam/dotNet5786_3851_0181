e71edb125f18b0da7d2bc13696012da5ee12b02d
David Uzan <dadouzan06@gmail.com>
2025-10-27 18:27:42 +0200

    partial class + Welcome function

---
+++ Stage0/Program0181.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage0
+{
+    partial class Program
+    {
+        // Defining declaration for the partial method
+        static partial void Welcome0181();
+        
+        static partial void Welcome0181() 
+        {
+            Console.WriteLine("Welcome to C#");
+        }
+    }
+}
