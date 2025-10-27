def7e62ddb53ce34076a68abe261c1fdfeffaf31
moshe mordechai diamandi <moshemor@g.jct.ac.il>
2025-10-27 19:24:31 +0200

    wassup

--- Stage0/Program0181.cs
+++ Stage0/Program0181.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace Stage0
@@ -14,6 +15,8 @@ namespace Stage0
         static partial void Welcome0181() 
         {
             Console.WriteLine("Welcome to C#");
+            Console.WriteLine("wassuppppp");
         }
+        
     }
 }
