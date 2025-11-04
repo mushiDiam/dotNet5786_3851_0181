d3a07ddd8317a8851a2e0b4c5f7a931c0c1129a5
moshe mordechai diamandi <moshemor@g.jct.ac.il>
2025-11-04 17:11:40 +0200

    update

---
+++ DalFacade/DalApi/IConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace DalApi;
+using DO;
+
+
+public interface IConfig
+{
+    DateTime Clock { get; set; }
+    int MaxRange { get; set; }
+
+    void Reset();
+}
---
+++ DalTest/Initialization.cs
@@ -0,0 +1,7 @@
+﻿namespace DalTest;
+using DalApi;
+using DO;
+
+public static class Initialization
+{
+}
