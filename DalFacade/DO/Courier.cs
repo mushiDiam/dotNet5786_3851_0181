290638a060a487e69f2b8fcba9529fbb77837bb5
David Uzan <dadouzan06@gmail.com>
2025-11-04 19:32:57 +0200

    Courier Record

--- DalFacade/DO/Courier.cs
+++ DalFacade/DO/Courier.cs
@@ -4,15 +4,14 @@ namespace Dal;
 
 public record Courier
 (
-    int Id,
-    bool Active,
-    double? MinDeliveryDistance,
-    DateTime DeliveryTime,
-    OrderType OrderType,
-    string Name = "",
-    string Phone = "",
-    string Email = "",
-    string Password = "",
-    string VehicleType = ""
+    int Id,//Courier's Id , natural key for the courier.
+    bool Active,//Is the courier active or not.
+    double? MaxDeliveryDistance,//Maximum distance the courier is willing to deliver.
+    DateTime DeliveryTime,//The time the delivery started.
+    OrderType OrderType,//How the courier delivers the order.
+    string Name = "",//Courier's name.
+    string Phone = "",//Courier's phone number.
+    string Email = "",//Courier's email.
+    string Password = ""//Courier's password.
 )
 { }
