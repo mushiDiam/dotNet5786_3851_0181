22402d6b073b7dcafbb30a01fa8e39b22d5177b9
Moshe Diamandi <moshemor@g.jct.ac.il>
2025-11-16 23:01:09 +0200

    chapter 7, making DalXml.cs and implementing IDal

---
+++ DalXml/DalXml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DalApi;
+
+namespace Dal;
+
+//stage 3
+sealed public class DalXml : IDal
+{
+	public IOrder Order { get; } = new OrderImplementation();
+	public ICourier Courier { get; } = new CourierImplementation();
+	public IDelivery Delivery { get; } = new DeliveryImplementation();
+	public IConfig Config { get; } = new ConfigImplementation();
+
+	public void resetDB(){
+		Order.DeleteAll();
+		Courier.DeleteAll();
+		Delivery.DeleteAll();
+		Config.Reset();
+	}
+}
