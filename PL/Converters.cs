07cd68eb7e84bf619b82359a89c3fea7429facd4
Moshe Diamandi <moshemor@g.jct.ac.il>
2026-01-04 19:09:24 +0200

    Add EnumToBrushConverter and improve courier grid styling
    Added EnumToBrushConverter for enum-to-color mapping in UI and integrated it into CourierListWindow.xaml to color the Transport column. Enhanced DataGrid styling for better text visibility and alignment. Removed several sample couriers from couriers.xml.

--- PL/Converters.cs
+++ PL/Converters.cs
@@ -2,6 +2,7 @@
 using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
+using System.Windows.Media;
 
 namespace PL
 {
@@ -59,5 +60,41 @@ namespace PL
         {
             throw new NotImplementedException();
         }
+    }
+    public class EnumToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null) return Brushes.Transparent;
+
+            // Map by enum name. Add/remove cases for other enum values you need.
+            return value.ToString() switch
+            {
+                // OrderStatus
+                "Open" => new SolidColorBrush(Color.FromRgb(0xFF, 0xF7, 0xE6)),         // pale orange
+                "InProgress" => new SolidColorBrush(Color.FromRgb(0xD0, 0xE9, 0xFF)),   // light blue
+                "Closed" => new SolidColorBrush(Color.FromRgb(0xDF, 0xF7, 0xDF)),       // light green
+                "Denied" => new SolidColorBrush(Color.FromRgb(0xFF, 0xC0, 0xC0)),       // light red
+                "Cancelled" => new SolidColorBrush(Color.FromRgb(0xEE, 0xEE, 0xEE)),    // light gray
+
+                // ScheduleStatus
+                "OnTime" => new SolidColorBrush(Color.FromRgb(0xE6, 0xFF, 0xE6)),
+                "Late" => new SolidColorBrush(Color.FromRgb(0xFF, 0xE0, 0xB2)),
+                "InRisk" => new SolidColorBrush(Color.FromRgb(0xFF, 0xF3, 0x8A)),
+
+                // Transportation / OrderType examples
+                "Car" => new SolidColorBrush(Color.FromRgb(0xD9, 0xEC, 0xFF)),
+                "Motorcycle" => new SolidColorBrush(Color.FromRgb(0xF0, 0xD9, 0xFF)),
+                "Bike" => new SolidColorBrush(Color.FromRgb(0xD9, 0xFF, 0xF0)),
+                "Walking" => new SolidColorBrush(Color.FromRgb(0xFF, 0xF7, 0xD9)),
+                "None" => new SolidColorBrush(Color.FromRgb(0xF3, 0xF3, 0xF3)),
+
+                // Default
+                _ => Brushes.Transparent,
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotSupportedException();
     }
 }
