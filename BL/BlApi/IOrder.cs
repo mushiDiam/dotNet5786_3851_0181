8bb5bd43117d9c4676bb705f6a7ba8a37347cf5d
David Uzan <dadouzan06@gmail.com>
2026-01-14 19:37:21 +0200

    Make order add/update async; refresh XML test data
    Refactored order add and update logic to be fully asynchronous, updating method signatures and internal geocoding calls. Refreshed all XML data files (orders, deliveries, couriers, config) with new and updated test/demo records. Also made geocoding in AdminImplementation explicitly async.

--- BL/BlApi/IOrder.cs
+++ BL/BlApi/IOrder.cs
@@ -6,10 +6,10 @@ public interface IOrder : IObservable{
     int[] CountByType(int id);
     IEnumerable<BO.OrderInList> GetOrders(int id, OrderInListOptions? filter,object? obj , OrderInListOptions? sort);
     BO.Order? Details(int id, int orderId);
-    void UpdateDetails(int id, BO.Order O);
+    Task UpdateDetails(int id, BO.Order O);
     void Cancel(int id, int orderId);
     void Delete(int id, int orderId);
-    void Add(int id, BO.Order O);
+    Task Add(int id, BO.Order O);
     void EndOfOrder(int id,int courierId, int orderId);
     void ChooseOrder(int id, int courierId, int orderId);
     IEnumerable<BO.ClosedDeliveryInList> GetEndedDeliveries(int id, int courierI, OrderTypes? filter, OrderInListOptions? sort );
